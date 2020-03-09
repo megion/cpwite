325952a1650cb454e322eefbfa007e541f0a46bb
Ilya Zadorozhny <ilya.zadorozhny@gmail.com>
2020-03-09 19:35:14 +0300

    add test for week1 tasks

--- src/test/week1/week1-tasks.cpp
+++ src/test/week1/week1-tasks.cpp
@@ -5,19 +5,98 @@ namespace week1 {
 
 /**
  * Задание по программированию: Второе вхождение
- */
+ *
+ * Дана строка. Найдите в этой строке второе вхождение буквы f
+ * и выведите индекс этого вхождения. Если буква f в данной строке
+ * встречается тольо один раз, выведите число -1,
+ * а если не встречается ни разу, выведите число -2. Индексы нумеруются с нуля.
+ * */
 void test_second_input()
 {
-    log(info, "start second_input");
     std::string in_str;
     std::cin >> in_str;
-    std::cout << in_str << std::endl;
+
+    int res = -2;
+    for (int i = 0; i < in_str.size(); i++) {
+        if (in_str[i] == 'f') {
+            if (++res == 0) {
+                res = i;
+                break;
+            }
+        }
+    }
+    std::cout << res << std::endl;
+}
+
+/**
+ * Задание по программированию: Наибольший общий делитель
+ *
+ * В stdin даны два натуральных числа.
+ * Выведите в stdout их наибольший общий делитель.
+ * */
+void test_greatest_common_divisor()
+{
+    int a, b;
+    std::cin >> a >> b;
+    // while (a > 0 && b > 0) {
+    if (a > b) {
+        a = a % b;
+    }
+    else {
+        b = b % a;
+    }
+    std::cout << a + b << std::endl;
+    // std::cin >> a >> b;
+    //}
+}
+
+/**
+ * Двоичные числа
+ *
+ * На вход дано целое положительное число N.
+ * Выведите его в двоичной системе счисления без ведущих нулей.
+ * */
+std::string transform_to_binary(int n)
+{
+    int p = 2; // основание системы счисления
+
+    /**
+     * while !(a<p)
+     * a = n/p // int part
+     * b = n - p * a // remainder
+     * next -> n = a
+     */
+    std::vector<int> res;
+    int a = p, b;
+    // std::cout << a << " ";
+    while (a > 0) {
+        a = n / p;
+        // std::cout << a << " ";
+        b = n - p * a; // here b = 0 or 1
+        n = a;
+        std::cout << b << " ";
+        res.push_back(b);
+    }
+
+    std::cout << std::endl;
+    std::cout << res << std::endl;
+
+    return res;
+}
+
+void test_transform_to_binary()
+{
+    assert(transform_to_binary(5) == "101");
+    assert(transform_to_binary(32) == "100000");
+    assert(transform_to_binary(1) == "1");
 }
 
 void week1_tasks()
 {
     suite("Week 1");
-    mytest(second_input);
+    // mytest(second_input);
+    // mytest(greatest_common_divisor);
+    mytest(transform_to_binary);
 }
 } // namespace week1
 } // namespace test
